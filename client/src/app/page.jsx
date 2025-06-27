e4d82fdd9992fdd5521b6e8f8428de0bb5e0bf7f
sushilhemrom500530 <sushilhemrom98@gmail.com>
2025-06-27 08:07:05 +0600

    add frontend initialize

---
+++ client/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-center text-3xl font-bold py-2">Hellow World!</h1>
+    </div>
+  );
+}
